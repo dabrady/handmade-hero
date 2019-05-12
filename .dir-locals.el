4614b6748637abdf9343620291ce9179d1b8d041
Daniel Brady <daniel.brady@tapjoy.com>
2019-05-12 17:37:34 +0900

    Local variables for Emacs

---
+++ .dir-locals.el
@@ -0,0 +1,2 @@
+((c++-mode . ((flycheck-gcc-include-path . ( "/Users/daniel.brady/bin/SDL2-2.0.9/include" ))
+              (flycheck-clang-include-path . ( "/Users/daniel.brady/bin/SDL2-2.0.9/include" )))))
