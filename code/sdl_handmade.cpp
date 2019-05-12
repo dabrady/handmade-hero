3a33b877462ea3da9c09e559467fb49ab327ee50
Daniel Brady <daniel.brady@tapjoy.com>
2019-05-12 18:18:26 +0900

    Initialize SDL

--- code/sdl_handmade.cpp
+++ code/sdl_handmade.cpp
@@ -1,10 +1,23 @@
 #include <SDL.h>
+#include <stdlib.h>
 
 int main(int argc, char** argv) {
-  SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_INFORMATION,
-                           "Handmade Hero",
-                           "This is handmade. I made it.",
-                           NULL);
+#if 1
+  SDL_LogSetAllPriority(SDL_LOG_PRIORITY_DEBUG);
+#endif
+
+  Uint32 subsystems =
+    SDL_INIT_VIDEO; // graphics and window management
+
+  // Initialize SDL
+  if (SDL_InitSubSystem(subsystems) != 0) {
+    SDL_LogCritical(SDL_LOG_CATEGORY_APPLICATION, "Unable to initialize SDL: %s", SDL_GetError());
+    return 1;
+  }
+  SDL_LogDebug(SDL_LOG_CATEGORY_APPLICATION, "SDL initialized");
+
+  // Shutdown SDL on exit
+  atexit(SDL_Quit);
 
   return 0;
 }
